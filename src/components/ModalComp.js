4444196b90bc5d22525f2e693d549421e3ebb3d6
franq01 <carreone705@gmail.com>
2023-08-01 19:47:55 -0600

    delete users

---
+++ src/components/ModalComp.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Modal, Header, Image, Button } from 'semantic-ui-react'
+
+const ModalComp = ({ open, setOpen, img, name, info, email, id, handleDelete, }) => {
+    return (
+        <Modal
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+            open={open}
+        >
+            <Modal.Header>user detail</Modal.Header>
+            <Modal.Content image>
+                <Image size='medium' src={img} wrapped />
+                <Modal.Description>
+                    <Header>{name}</Header>
+                    <p>{email}</p>
+                    <p>{info}</p>
+
+                </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+                <Button color='black' onClick={() => setOpen(false )}>
+                    cancel
+                </Button>
+                <Button 
+                color='red' 
+                content="Delete" 
+                labelPosition='right'
+                icon="checkmark" 
+                onClick={()=> handleDelete(id)}/> 
+            </Modal.Actions>
+
+        </Modal>
+    )
+}
+
+export default ModalComp
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,74 +1,101 @@
-import React,{useState, useEffect} from 'react'
-import{ db} from'../firebase/firebase'
-import{Button, Card, Container, Grid, Image, Item} from 'semantic-ui-react'
-import {useNavigate} from 'react-router-dom'
-import {collection, doc, onSnapshot, snapshotEqual} from 'firebase/firestore'
+import React, { useState, useEffect } from 'react'
+import { db } from '../firebase/firebase'
+import { Button, Card, Container, Grid, Image, } from 'semantic-ui-react'
+import { useNavigate } from 'react-router-dom'
+import { collection, deleteDoc, doc, onSnapshot, snapshotEqual } from 'firebase/firestore'
+import ModalComp from '../components/ModalComp'
+
 
 const Home = () => {
     const [users, setUsers] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [user, setUser] = useState({});
     const [loading, setLoading] = useState(false);
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
 
     useEffect(() => {
         setLoading(true);
-        const unsub = onSnapshot(collection(db, "users"), (snapshot) =>{
-            let list = [   ];
-            snapshot.docs.forEach((doc) =>{
-                list.push({id: doc.id, ...doc.data()})
+        const unsub = onSnapshot(collection(db, "users"), (snapshot) => {
+            let list = [];
+            snapshot.docs.forEach((doc) => {
+                list.push({ id: doc.id, ...doc.data() })
             });
             setUsers(list);
             setLoading(false);
 
         },
-        (error)=>{
-            console.error(error);
-        }
+            (error) => {
+                console.error(error);
+            }
         );
-        return ()=>{
+        return () => {
             unsub();
         };
-    },[]);
-  return (
-    <Container>
-        <Card.Group>
-            <Grid columns={3} stackable
-            >
-                {users && users.map((item)=>(
-                    <Grid.Column>
-                        <Card key={item.id}>
-                            <Card.Content>
-                                <Image
-                                src={item.img}
-                                size='medium'
-                                style={{
-                                    height:"150px",
-                                    width:"150px",
-                                    borderRadious:"50%",
-                                }}
-                                />
-                                <Card.Header style={{marginTop: "10px"}}>
-                                    {item.name}
-                                </Card.Header>
-                                <Card.Description >{item.info}</Card.Description>
-                            </Card.Content>
-                            <Card.Content extra>
-                                <div>
-                                    <button color='green' onClick={()=> navigate(`/update/${item.id}`)}>
-                                        update
-                                    </button>
-                                    <button color='purple' >
-                                        view
-                                    </button>
-                                </div>
+    }, []);
+    const handleModal = (item) => {
+        setOpen(true);
+        setUser(item);
+    };
 
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
-                ))}
-            </Grid>
-        </Card.Group>
-    </Container>
-  )
+    const handleDelete = async (id) => {
+        if (window.confirm("are you sure to delete that user?")) {
+            try {
+                setOpen(false);
+                await deleteDoc(doc(db, "users", id));
+                setUsers(users.filter((user) => user.id !== id));
+            } catch (err) { console.log(err) }
+        }
+    }
+    return (
+        <Container>
+            
+                <Grid columns={3} stackable
+                >
+                    {users && users.map((item) => (
+                        <Grid.Column key={item.id}>
+                            <Card >
+                                <Card.Content>
+                                    <Image
+                                        src={item.img}
+                                        size='medium'
+                                        style={{
+                                            height: "150px",
+                                            width: "150px",
+                                            borderRadious: "50%",
+                                        }}
+                                    />
+                                    <Card.Header style={{ marginTop: "10px" }}>
+                                        {item.name}
+                                    </Card.Header>
+                                    <Card.Description >{item.info}</Card.Description>
+                                </Card.Content>
+                                <Card.Content extra>
+                                    <div>
+                                        <Button color='green' onClick={() => navigate(`/update/${item.id}`)}>
+                                            update
+                                        </Button>
+                                        <Button
+                                            color='purple'
+                                            onClick={() => handleModal(item)}
+                                        >view</Button>
+                                        {open && (
+                                            <ModalComp
+                                                open={open}
+                                                setOpen={setOpen}
+                                                handleDelete={handleDelete}
+                                                {...user}
+                                            />
+                                        )}
+                                    </div>
+
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                    ))}
+                </Grid>
+            
+        </Container>
+    )
 }
 
 export default Home
